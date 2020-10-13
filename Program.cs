34d1f8cecb74d093b78b3a69925386b61e0e5b8d
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2020-10-13 13:56:40 -0700

    udpate comment vat

--- Program.cs
+++ Program.cs
@@ -8,36 +8,49 @@ namespace Treehouse.PracticeSession
         public static void Main()
         {
 
-  ShoppingCart sc = new ShoppingCart();
+            Cart cart = new Cart();
 
-            var item1 = new Collections.Item();
-            item1.Key = 1;
-            item1.ItemName = "Huawei V8";
-            sc.Add(item1);
+            bool stillAddingItems = true;
 
-            var item2 = new Collections.Item();
-            item2.Key = 2;
-            item2.ItemName = "Huawei V9";
-            sc.Add(item2);
-
-            var item3 = new Collections.Item();
-            item3.Key = 3;
-            item3.ItemName = "Huawei V10";
-            sc.Add(item3);
-
-            Console.WriteLine("使用索引器，输出对象：");
-            Console.WriteLine(sc[3].ToString());
-
-            Console.WriteLine("集合中对象数量：");
-            Console.WriteLine(sc.Count);
-
-            Console.WriteLine("列出所有对象：");
-            sc.Items.ForEach(delegate (Collections.Item item)
+            while(stillAddingItems)
             {
-                Console.WriteLine(item.ToString());
-            });
-
+                Console.Clear();
+                Console.WriteLine("What would you like to do? ");
+                Console.WriteLine("Type 'add' to add an item.");
+                Console.WriteLine("Type 'Checkout' to show your list.");
+                string response = Console.ReadLine();
+                Console.Clear();
+                switch (response)
+                {
+                    case "add":
+                        {
+                            Console.Write("What item would you like to add? ");
+                            string whatTheUserTyped = Console.ReadLine();
+                            Console.Write("What is the price of the item ");
+                            double price = Convert.ToInt32(Console.ReadLine());
+                            cart.AddToCart(whatTheUserTyped);
+                            break;
+                        }
             
+                    case "Checkout":
+                        {
+                            double total = price +  * 1.15 * price<list>;
+
+                            foreach (string itemName in cart.GetCartIteminfo())
+                            {
+                                Console.WriteLine(itemName);
+    
+                            }
+                            Console.WriteLine("the total is " total )
+                            
+                            Console.WriteLine("Press enter to continue");
+                            Console.ReadLine();
+                            break;
+                        }
+
+                }
+            }
+
         }
     }
 }
